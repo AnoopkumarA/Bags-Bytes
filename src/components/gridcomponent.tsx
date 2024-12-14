bdfdc205900022b02f58ed0d3dfc5fe65c7a64c8
Pillzen Pvt. Ltd <anopadraja@gmail.com>
2024-12-14 23:14:14 +0530

    update

--- src/components/gridcomponent.tsx
+++ src/components/gridcomponent.tsx
@@ -1,7 +1,7 @@
 import '../App.css'
 export function GridBackgroundDemo() {
     return (
-      <div className="h-[10.4rem] md:h-[29rem] w-full bg-navy-900  dark:bg-grid-white/[0.2] bg-grid-white/[0.2] bg-grid-custom relative flex items-center justify-center -mt-96 top-48  ">
+      <div className="h-[4.4rem] md:h-[29rem] w-full bg-navy-900  dark:bg-grid-white/[0.2] bg-grid-white/[0.2] bg-grid-custom relative flex items-center justify-center -mt-96 top-48  ">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center  bg-navy-900 [mask-image:radial-gradient(ellipse_at_center,transparent_-60%,black)]"></div>
       
